b1ea571c467c679a0e5892507f0fa33a011302e3
fontainej <fontainej@gmail.com>
2015-05-25 23:35:30 +0200

    Added comments.

--- src/assets/js/eventListeners.js
+++ src/assets/js/eventListeners.js
@@ -1,10 +1,12 @@
 (function () {
     'use strict';
 
+    // Scroll event handler for the sticky header
     window.addEventListener('scroll', function () {
         document.getElementsByTagName('header')[0].className = (window.scrollY > 1) ? 'sticky' : '';
     });
 
+    // Click event handler for the menu side drawer
     document.getElementById('menu-icon').addEventListener('click', function () {
         var elem = document.getElementById('sidebar');
         elem.className = (elem.className.indexOf('open')) ? 'open' : '';
